5c94440e5f0690c257d4ae3904652e0a3b51ebfd
03yadavankit <03yadavankit@gmail.com>
2024-06-01 10:56:11 +0530

    Now it will start uploading your files to your GitHub repository.

---
+++ frontend/src/customer/Components/Auth/Login.jsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import { Grid, TextField, Button, Box, Snackbar, Alert } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, login } from "../../../Redux/Auth/Action";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function LoginUserForm({ handleNext }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const [openSnackBar, setOpenSnackBar] = useState(false);
+  const [loginError, setLoginError] = useState(null);
+  const { auth } = useSelector((store) => store);
+  const handleCloseSnakbar = () => {
+    setOpenSnackBar(false);
+    setLoginError(null);
+  };
+
+  useEffect(() => {
+    if (jwt) {
+      dispatch(getUser(jwt));
+    }
+  }, [jwt]);
+
+  useEffect(() => {
+    if (auth.user || auth.error) setOpenSnackBar(true);
+  }, [auth.user, auth.error]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const userData = {
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+
+    try {
+      await dispatch(login(userData));
+      setLoginError(null); // Reset login error on successful login
+    } catch (error) {
+      setLoginError("Invalid email or password"); // Set the login error message
+    }
+  };
+
+  return (
+    <React.Fragment className=" shadow-lg ">
+      <form className="w-full" onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="given-name"
+              type="password"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              className="bg-[#9155FD] w-full"
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ padding: ".8rem 0" }}
+            >
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <div className="flex justify-center flex-col items-center">
+        <div className="py-3 flex items-center">
+          <p className="m-0 p-0">Don't have an account?</p>
+          <Button onClick={() => navigate("/register")} className="ml-5" size="small">
+            Register
+          </Button>
+        </div>
+      </div>
+      <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleCloseSnakbar}>
+        <Alert onClose={handleCloseSnakbar} severity={loginError ? "error" : "success"} sx={{ width: '100%' }}>
+          {loginError ? loginError : auth.user ? "Login Success" : ""}
+        </Alert>
+      </Snackbar>
+    </React.Fragment>
+  );
+}
